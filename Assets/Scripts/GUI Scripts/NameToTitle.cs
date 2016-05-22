e26b066cd2a5cc92b1925cfcbd8f2067080ebdd2
Hmpflabul <0chomar0@gmail.com>
2016-05-22 16:11:32 +0200

    first final

--- Assets/Scripts/GUI Scripts/NameToTitle.cs
+++ Assets/Scripts/GUI Scripts/NameToTitle.cs
@@ -7,27 +7,30 @@ public class NameToTitle : MonoBehaviour {
 	public Text title;
 
 
+    void Start()
+    {
+    }
 	void OnMouseEnter()
 	{
 		switch(name)
 		{
-		case "Pac-Man":
+		case "HMPFLABUL":
 			title.color = Color.yellow;
 			break;
 
-		case "Blinky":
+		case "HOBBYS":
 			title.color = Color.red;
 			break;
 
-		case "Pinky":
+		case "STUDY":
 			title.color = new Color(254f/255f, 152f/255f, 203f/255f);
 			break;
 
-		case "Inky":
+		case "GYM":
 			title.color = Color.cyan;
 			break;
 
-		case "Clyde":
+		case "CHORES":
 			title.color = new Color(254f/255f, 203f/255f, 51f/255f);
 			break;
 		}
